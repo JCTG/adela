10a436bf01bed716f5194c013d75f77d927b6660
JCTG <juan.tovar@vun.mx>
2017-02-20 16:31:19 -0600

    prueba de jenkins con error en unit test

--- spec/models/dataset_spec.rb
+++ spec/models/dataset_spec.rb
@@ -27,7 +27,7 @@ describe Dataset do
     end
 
     it 'should not be valid without a title' do
-      dataset.title = nil
+      dataset.title = "Mi titulo"
       expect(dataset).not_to be_valid(:inventory)
     end
 
