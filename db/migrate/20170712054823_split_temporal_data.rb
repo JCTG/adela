b8974f769b72c477f0fd2ffdb211c1d3d9f8a604
Luis Alfredo Lorenzo <babasbot@gmail.com>
2017-07-12 01:06:54 -0500

    Splits the temporal data into the init_date & term_date column

---
+++ db/migrate/20170712054823_split_temporal_data.rb
@@ -0,0 +1,33 @@
+class SplitTemporalData < ActiveRecord::Migration
+  def change
+    Dataset.where.not(temporal: nil).map do |dataset|
+      begin
+        temporal = dataset.temporal.split('/').map do |date|
+          ISO8601::Date.new(date)
+        end
+
+        dataset.update(
+          temporal_init_date: temporal[0].to_s,
+          temporal_init_date: temporal[1].to_s
+        )
+      rescue ISO8601::Errors::UnknownPattern
+        next
+      end
+    end
+
+    Distribution.where.not(temporal: nil).map do |distribution|
+      begin
+        temporal = distribution.temporal.split('/').map do |date|
+          ISO8601::Date.new(date)
+        end
+
+        distribution.update(
+          temporal_init_date: temporal[0].to_s,
+          temporal_init_date: temporal[1].to_s
+        )
+      rescue ISO8601::Errors::UnknownPattern
+        next
+      end
+    end
+  end
+end
