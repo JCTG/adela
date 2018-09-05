3a1e2a86605441941a64cfcb1a0b8355f94f65fa
=jctg <=juan.tovar@vun.mx>
2018-09-05 14:12:56 -0500

    Fix error upload Memo_File and Designation_File

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -1,5 +1,8 @@
+require 'net/ftp'
+
 module Admin
-  class OrganizationsController < Admin::BaseController
+  class OrganizationsController < Admin::BaseController 
+
     load_and_authorize_resource except: [:update_multiple]
 
     def index
@@ -18,7 +21,6 @@ module Admin
         flash[:notice] = I18n.t('flash.notice.organization.create')
         redirect_to admin_organizations_path
       else
-        # flash[:alert] = I18n.t('flash.alert.organization.create')
         render 'new'
       end
       
@@ -35,17 +37,57 @@ module Admin
       @organization = Organization.find(params[:id])
       @title = @organization.title
       oldname = @organization.slug
+
       if @organization.update(organization_params)
         flash[:notice] = I18n.t('flash.notice.organization.update')
-
-        unless @title == params[:title]
-          newname = @organization.slug
-          ShogunUpdateorgWorker.perform_async(oldname, newname, @organization.title)
-        end
+            unless @title == params[:title]
+                newname = @organization.slug
+                ShogunUpdateorgWorker.perform_async(oldname, newname, @organization.title)  
+                if oldname != newname 
+                  cambio_org_ftp oldname, newname
+                end        
+            end  
         redirect_to admin_organizations_path
       else
-        # flash[:alert] = I18n.t('flash.alert.organization.update')
         render 'edit'
+      end
+    end
+
+    def cambio_org_ftp oldname , newname 
+     begin
+      path_base_desg_file = ENV['BASE_DESIGNATION_FILE']
+      path_base_memo_file = ENV['BASE_MEMO_FILE']
+      file_dir            = ENV['FOLDER_FILE']   
+      old_path_designation_file = "#{path_base_desg_file}" + "#{oldname}" + "#{file_dir}"
+      new_path_designation_file = "#{path_base_desg_file}" + "#{newname}" + "#{file_dir}"
+      old_path_memo_file        = "#{path_base_memo_file}" + "#{oldname}" + "#{file_dir}"
+      new_path_memo_file        = "#{path_base_memo_file}" + "#{newname}" + "#{file_dir}"
+     
+      create_new_structure_ftp  oldname , newname , old_path_designation_file
+      create_new_structure_ftp  oldname , newname , old_path_memo_file
+      copy_files old_path_designation_file , new_path_designation_file , oldname , newname
+      copy_files old_path_memo_file        , new_path_memo_file        , oldname , newname
+      delete_old_estructure_ftp oldname , newname , old_path_designation_file
+      delete_old_estructure_ftp oldname , newname , old_path_memo_file
+      msg = nil
+      rescue EOFError => e
+          msg = e.message 
+      rescue SocketError => e
+          msg = e.message
+      rescue OpenURI::HTTPError => e
+          msg = e.message 
+      rescue Net::FTPPermError => e
+          msg = e.message 
+      rescue Net::SFTP::StatusException => e
+          msg = e.message 
+      rescue Errno::ETIMEDOUT => e
+          msg = e.message 
+      rescue Exception => e
+          msg = e.message
+      end
+
+      if msg != nil
+          gen_log_file oldname , newname , msg
       end
     end
 
@@ -71,6 +113,94 @@ module Admin
         administrator_attributes: [:user_id],
         liaison_attributes: [:user_id],
         organization_sectors_attributes: [:id, :sector_id, :_destroy])
+    end
+
+    def create_new_structure_ftp oldname , newname , path
+    Net::FTP.open(ENV['FTP_HOST'], user= ENV['FTP_USER'], passwd= ENV['FTP_PASSWD']) do |ftp|
+      if (ftp.dir(path).length > 0 ) and ( oldname != newname )   
+          ftp.chdir(path)
+          ftp.binary = true
+          old_path   = ftp.pwd
+          folders    = []
+          indice     = 0 
+          estructura = ftp.nlst("*")
+          folder_file = "file"
+          estructura.each do |elem|    
+              folders[indice]  = elem.split('/').first
+              indice += 1 
+          end
+          ftp.chdir('../../')
+          ftp.mkdir(newname)
+          ftp.chdir(newname)      
+          ftp.mkdir(folder_file)
+          ftp.chdir(folder_file)
+          folders.each do |folder|  
+            ftp.mkdir(folder)
+          end
+        end
+      ftp.close  
+    end
+    end
+
+    def  copy_files old_path , new_path , oldname , newname
+      Net::FTP.open(ENV['FTP_HOST'], user= ENV['FTP_USER'], passwd= ENV['FTP_PASSWD']) do |ftp|      
+        if (ftp.dir(old_path).length > 0 ) and ( oldname != newname )
+            ftp.chdir(old_path)
+            files  = [] 
+            indice = 0
+            estructura = ftp.nlst("*")
+            estructura.each do |elem|    
+               files[indice]    = elem.split('/').first + "/" + elem.split('/').last
+                indice += 1 
+            end
+            files.each do |elem|
+              origen   = "#{old_path}" + "/" + elem 
+              destino  = "#{new_path}" + "/" + elem  
+              ftp.rename(origen, destino)
+            end
+           ftp.close
+          end  
+      end  
+    end
+
+    def delete_old_estructure_ftp oldname , newname ,path
+      Net::FTP.open(ENV['FTP_HOST'], user= ENV['FTP_USER'], passwd= ENV['FTP_PASSWD']) do |ftp|   
+        if (ftp.dir(path).length > 0 ) and ( oldname != newname )
+          ftp.chdir(path)
+          estructura =  ftp.nlst()
+          estructura.each { |folder|
+            ftp.rmdir(folder)  
+          }
+          ftp.chdir("../")
+          ftp.rmdir("file")
+          ftp.chdir("../")
+          ftp.rmdir(oldname)   
+        end
+        ftp.close
+      end   
+    end
+   
+    def check_nueva_est_creada_ftp old_path_designation_file , old_path_memo_file
+      existeOldStructure = false
+      Net::FTP.open(ENV['FTP_HOST'], user= ENV['FTP_USER'], passwd= ENV['FTP_PASSWD']) do |ftp|   
+        if ftp.dir(old_path_designation_file).length == 0 and ftp.dir(old_path_memo_file).length  == 0
+             existeOldStructure = true
+        end
+        ftp.close
+      end
+      return existeOldStructure   
+    end
+      
+    def  gen_log_file oldname , newname , msg 
+       Net::FTP.open(ENV['FTP_HOST'], user= ENV['FTP_USER'], passwd= ENV['FTP_PASSWD']) do |ftp|   
+        fileName =  "oldname_" + oldname + "_newname_" + newname + "_" +  "#{Time.parse(DateTime.now.to_s)}".gsub!(" ", "-")
+        f = File.new("/tmp/#{fileName}", 'a')
+        f.write(msg)
+        f.close
+        ftp.chdir(ENV['PATH_LOGS_CAMBIO_ORG'])      
+        ftp.put("/tmp/#{fileName}")
+        ftp.close
+       end
     end
   end
 end
---
+++ config/initializers/cambio_org.rb
@@ -0,0 +1,5 @@
+#rutas de FTP pantalla Documentos
+ENV['BASE_DESIGNATION_FILE'] = ENV['BASE_DESIGNATION_FILE'] || '/420657/datos/adela_qa2017/uploads/designation_file/'
+ENV['BASE_MEMO_FILE']        = ENV['BASE_MEMO_FILE'] 		|| '/420657/datos/adela_qa2017/uploads/memo_file/'
+ENV['FOLDER_FILE']           = ENV['FOLDER_FILE']           || '/file'
+ENV['PATH_LOGS_CAMBIO_ORG']  = ENV['PATH_LOGS_CAMBIO_ORG']  || '/420657/datos/adela_qa2017/uploads/logs_up_camb_org'
